e204f113ebcc701673bfcc30f60acb945db2f045
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-02-09 13:51:51 -0300

    fix an error

--- lua/search-replace/init.lua
+++ lua/search-replace/init.lua
@@ -21,45 +21,50 @@ local function escapeForRegex(x)
 	)
 end
 
-M.searchAndReplace = function()
-	local currPos = vim.fn.col(".")
-	local currLine = vim.fn.line(".")
-	local startVisualPos = vim.fn.getpos("v")[3]
-	local vSelection = string.sub(currLine, startVisualPos, currPos)
-	local seq_cur = vim.fn.undotree().seq_cur
+M.searchAndReplace = function(motion)
+	print("setting map for " .. motion)
+	vim.keymap.set("v", motion, function()
+		local currPos = vim.fn.col(".")
+		local currLine = vim.fn.line(".")
+		local startVisualPos = vim.fn.getpos("v")[3]
+		local vSelection = string.sub(vim.api.nvim_get_current_line(), startVisualPos, currPos)
+		local seq_cur = vim.fn.undotree().seq_cur
 
-	local okGetReplaceString, replaceString = pcall(vim.fn.input, "Replace: ", vSelection)
+		local okGetReplaceString, replaceString = pcall(vim.fn.input, "Replace: ", vSelection)
 
-	execute("normal<Esc>")
+		execute("normal<Esc>")
 
-	if not okGetReplaceString then
-		return
-	end
+		if not okGetReplaceString then
+			return
+		end
 
-	local search = escapeForRegex(vSelection)
-	local replace = escapeForRegex(replaceString)
+		local search = escapeForRegex(vSelection)
+		local replace = escapeForRegex(replaceString)
 
-	-- `:h range` or `:h substitute` to see more config options
-	local searchAndReplaceInline = "s/\\%>" .. startVisualPos - 1 .. "c" .. search .. "/" .. replace .. "/gcI"
-	local searchAndReplaceNext = currLine + 1 .. ",$s/" .. search .. "/" .. replace .. "/gcI"
+		-- `:h range` or `:h substitute` to see more config options
+		local searchAndReplaceInline = "s/\\%>" .. startVisualPos - 1 .. "c" .. search .. "/" .. replace .. "/gcI"
+		local searchAndReplaceNext = currLine + 1 .. ",$s/" .. search .. "/" .. replace .. "/gcI"
 
-	local ok, _ = pcall(execute, searchAndReplaceInline)
+		local ok, _ = pcall(execute, searchAndReplaceInline)
 
-	if not ok then
-		vim.cmd("undo " .. seq_cur)
+		if not ok then
+			vim.cmd("undo " .. seq_cur)
+			execute("nohlsearch")
+			return
+		end
+
+		local ok2, msg2 = pcall(execute, searchAndReplaceNext)
+
+		local patternFound = msg2 ~= nil and string.find(msg2, "Pattern not found")
+
+		if not ok2 and not patternFound then
+			vim.cmd("undo " .. seq_cur)
+			execute("nohlsearch")
+			return
+		end
+
 		execute("nohlsearch")
-		return
-	end
-
-	local ok2, _ = pcall(execute, searchAndReplaceNext)
-
-	if not ok2 then
-		vim.cmd("undo " .. seq_cur)
-		execute("nohlsearch")
-		return
-	end
-
-	execute("nohlsearch")
+	end)
 end
 
 return M
